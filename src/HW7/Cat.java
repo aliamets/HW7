fbce50d1de6bc14b024af75b7be0d5a4572b6705
Лямец Алексей <65312091+aliamets@users.noreply.github.com>
2020-06-05 10:25:02 +0300

    HW7

--- src/HW7/Cat.java
+++ src/HW7/Cat.java
@@ -1,5 +1,29 @@
 package HW7;
 
 public class Cat {
+    private String name;
+    private int appetite;
 
+    public boolean satiety = false;
+
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void eat(Plate plate) {
+        if (satiety == true) {
+            System.out.println(name +": Не голоден");
+        } else if (this.appetite > plate.getFood()) {
+            System.out.println(name +": Мало еды");
+        } else {
+            System.out.println(name + ": поел");
+            plate.decreaseFood(this.appetite);
+
+        }
+    }
 }
